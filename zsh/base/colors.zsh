5dc2929c32e36d3ab084e4594fd340d96e8359eb
Giuliano Schneider <gs93@gmx.net>
2013-03-06 03:14:24 +0100

    made options more dynamic

--- zsh/base/colors.zsh
+++ zsh/base/colors.zsh
@@ -3,7 +3,10 @@ autoload -U colors && colors
 
 if [ "$TERM" != "dumb" ]; then
     eval "`dircolors -b`"
-    alias ls='ls -hF --color=auto'
-    export GREP_OPTIONS='--color=auto'
-    export LESS='-R'
+    export GREP_OPTIONS="$GREP_OPTIONS --color=auto"
+    export LESS="$LESS -R"
+
+    # ls doesn't support export
+    export LS_OPTIONS="$LS_OPTIONS --color=auto"
+    alias ls="ls $LS_OPTIONS"
 fi
--- zsh/base/readable.zsh
+++ zsh/base/readable.zsh
@@ -2,3 +2,7 @@ alias du='du -h'
 alias df='df -h'
 alias free='free -h'
 alias rsync='rsync -h'
+
+# ls doesn't support export
+export LS_OPTIONS="$LS_OPTIONS -hF"
+alias ls="ls $LS_OPTIONS"
