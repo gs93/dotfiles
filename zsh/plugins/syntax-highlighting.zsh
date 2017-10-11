a4939cb849d516cac8c54fdad85d95437f1d39ca
Giuliano Schneider <gs93@gmx.net>
2017-10-11 21:13:54 +0200

    zsh: mark more commands red

--- zsh/plugins/syntax-highlighting.zsh
+++ zsh/plugins/syntax-highlighting.zsh
@@ -2,8 +2,12 @@ if [[ -s /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.
     source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
     ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor)
     ZSH_HIGHLIGHT_STYLES[commandseparator]=fg=blue
-    ZSH_HIGHLIGHT_PATTERNS+=('rm -rf *' 'fg=white,bold,bg=red')
-    ZSH_HIGHLIGHT_PATTERNS+=('chown -R *' 'fg=white,bold,bg=red')
-    ZSH_HIGHLIGHT_PATTERNS+=('chmod -R *' 'fg=white,bold,bg=red')
+
+    local warnCol='fg=white,bold,bg=red'
+    ZSH_HIGHLIGHT_PATTERNS+=('rm -rf *' $warnCol)
+    ZSH_HIGHLIGHT_PATTERNS+=('chown -R *' $warnCol)
+    ZSH_HIGHLIGHT_PATTERNS+=('chmod -R *' $warnCol)
+    ZSH_HIGHLIGHT_PATTERNS+=('shutdown' $warnCol)
+    ZSH_HIGHLIGHT_PATTERNS+=('reboot' $warnCol)
 fi
 
